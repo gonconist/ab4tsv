b62985bcf2b790c80c37e988e7244b72f6839467
gonconist <32281275+gonconist@users.noreply.github.com>
2022-03-30 13:19:28 +0200

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+A=tgt
+B=hyps
+C=def
+D=hyps
+encoding=swap_fc              # -e
+target_fc=$                   # -tfc
+hyps_start_em=$               # -hem1
+hyps_end_em=$                 # -hem2
+permutation_invariance=False  # -pi
+
 seed=42                       # --seed
 num_epochs=5                  # --num_epochs
 batch_size_train=16           # --batch_size_train
 batch_size_val=8              # --batch_size_val
-A=cls
-B=cls
-C=descr
-D=descr
-encoding=swap_fc_plus         # -e
-target_fc=$                   # -tfc
-hyps_starting_fc=$            # -hfc1
-hyps_ending_fc=$              # -hfc2
-permutation_invariance=False  # -pi
 output_dir=results            # --output_dir
 
 python source/train.py -a $A $B $C $D -e $encoding -pi $permutation_invariance

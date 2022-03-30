289528f74f7f266d89561ae6b3e0ba2923f564fc
gonconist <32281275+gonconist@users.noreply.github.com>
2022-03-30 13:22:47 +0200

    Update eval.sh

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-A=cls
-B=cls
-C=descr				
-D=descr
+A=tgt
+B=hyps
+C=def
+D=hyps
 dataset=dev                     # -d
-encoding=swap_fc_plus         	# -e
+encoding=swap_fc               	# -e
 target_fc=$                   	# -tfc
 hyps_starting_fc=$            	# -hfc1
 hyps_ending_fc=$              	# -hfc2
-permutation_invariance=False	# -pi
-save_preds=True			# --save_preds
-out_binary_preds=True		# --out_binary_preds	
+permutation_invariance=False	  # -pi
+save_preds=True			            # --save_preds
+out_binary_preds=False		      #--out_binary_preds	
 output_dir=results            	# --output_dir
 
-python source/eval.py -a $A $B $C $D -d $dataset -e $encoding -pi $permutation_invariance --save_preds $save_preds --out_binary_preds $out_binary_preds
+python source/eval.py -a $A $B $C $D -d $dataset -e $encoding -pi $permutation_invariance
